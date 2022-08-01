4fdcc1c9a87b3937cf905ee6096e386c68d3062d
Miguel E. Cabrera <mecg1994@gmail.com>
2022-07-31 21:47:57 -0600

    pasword

---
+++ pasword.py
@@ -0,0 +1,18 @@
+#PASSWORD GENERATOR
+
+
+import random
+
+
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number ="0123456789"
+symbols =".#$%&/!@"
+
+
+use_for = lower_case+ upper_case + number + symbols 
+password_text = 8
+
+password = "".join(random.sample(use_for,password_text) )
+
+print("Tu contraseña es:  "+  password)
