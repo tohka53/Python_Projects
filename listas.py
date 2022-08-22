9932a4f701867917a7e92d9ba0c3c1470e2fcacc
Miguel E. Cabrera <mecg1994@gmail.com>
2022-08-22 16:02:00 -0600

    day 4 half

---
+++ listas.py
@@ -0,0 +1,5 @@
+departamentos_guatmeala = ["Guatemala" ,"Escluintla","Peten"]
+departamentos_guatmeala.append("San Marcos")
+
+
+print(departamentos_guatmeala)
---
+++ split.py
@@ -0,0 +1,4 @@
+nombres_listado = input("Ingrese el listado de nombre de personas listados por comas y un espacio como se muestra en el ejemplo: Miguel, Eduardo, Juan")
+
+names= nombres_listado.split(", ")
+print(names)
