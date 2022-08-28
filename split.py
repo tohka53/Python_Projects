28e9f82d0e008e3379ca3aa20e9b1298404efa94
Miguel E. Cabrera <mecg1994@gmail.com>
2022-08-28 16:53:06 -0600

    Update split.py

--- split.py
+++ split.py
@@ -1,4 +1,9 @@
+from unicodedata import name
+
+import random
 nombres_listado = input("Ingrese el listado de nombre de personas listados por comas y un espacio como se muestra en el ejemplo: Miguel, Eduardo, Juan")
 
 names= nombres_listado.split(", ")
-print(names)
+total_nombre = len(names)
+random_name = random.randint(0, total_nombre-1)
+print(names[random_name])
