e8cc283d79b476b7a26255644c05086c9181aed9
Miguel E. Cabrera <mecg1994@gmail.com>
2022-09-04 12:38:23 -0600

    part3 day 5

---
+++ gaus.py
@@ -0,0 +1,7 @@
+from unicodedata import numeric
+
+
+total = 0
+for number in range(1,101):
+    total += number
+print(total)
---
+++ pareoimpares.py
@@ -0,0 +1,4 @@
+total = 0
+for number in range(1,100):
+   if number %2 !=0:
+    print(number)
