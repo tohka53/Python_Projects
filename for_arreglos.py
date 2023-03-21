81562d46145b20dba03469eff8d28f23bd37f517
Miguel E. Cabrera <mecg1994@gmail.com>
2023-03-20 20:42:40 -0600

    qer

--- for_arreglos.py
+++ for_arreglos.py
@@ -1,1 +1,30 @@
-arreglo = []
+from statistics import LinearRegression
+import cx_Oracle
+import mysql.connector
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+# Conectar a la base de datos de Oracle
+#conn = cx_Oracle.connect('user/password@host:port/service')
+cnx = mysql.connector.connect(user='root', password='',
+                              host='127.0.0.1',
+                              database='db_personal_record')
+# Obtener los datos necesarios para hacer la predicción desde la base de datos de Oracle
+query = "SELECT nombre_completo, email, numero_telefonico FROM tb_clientes"
+df = pd.read_sql(query, con=cnx)
+
+# Preprocesar los datos
+# ...
+
+# Entrenar un modelo de regresión lineal utilizando los datos preprocesados
+X = df[['email', 'numero_telefonico']]
+y = df['nombre_completo']
+model = LinearRegression().fit(X, y)
+
+# Utilizar el modelo entrenado para hacer predicciones en nuevos datos
+new_data = pd.DataFrame({'email': [1, 2, 3], 'numero_telefonico': [4, 5, 6]})
+predictions = model.predict(new_data)
+
+# Guardar las predicciones en la base de datos de Oracle
+# ...
---
+++ qr.py
@@ -0,0 +1,16 @@
+import qrcode
+
+# Creamos el objeto QRCode
+qr = qrcode.QRCode(version=1, box_size=10, border=5)
+
+# Agregamos la información que queremos codificar
+qr.add_data("https://www.instagram.com/mecg53/")
+
+# Generamos el código QR
+qr.make(fit=True)
+
+# Creamos una imagen del código QR
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Guardamos la imagen del código QR
+img.save("instagramqr.png")
