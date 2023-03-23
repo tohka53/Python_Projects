e4be10ef8e848032e56227fb7fe5779cbab2d192
Miguel E. Cabrera <mecg1994@gmail.com>
2023-03-23 12:43:14 -0600

    Update qr2.py

--- qr2.py
+++ qr2.py
@@ -1,4 +1,21 @@
+#Import module
 import qrcode
-img = qrcode.make('Some data here')
-type(img)  # qrcode.image.pil.PilImage
-img.save("some_file.png")
+
+
+print("QR CODE GENERATOR")
+a= input("Enter link which you of want to make qr code")
+#set QR code parameteres
+qr = qrcode.QRCode(
+    version = 10,
+    box_size = 5,
+    border = 1
+)
+#storing input given into data variable
+data = a
+qr.add_data(data)
+#Generate QR Code
+qr.make(fit = True)
+#set QR code properties
+img = qr.make_image(fill="black",back_color = "white")
+#save QR code
+img.save("QrCodeImage.png")
